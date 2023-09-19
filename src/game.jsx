a46458d1ef5dd6fed6e6d4e8b012a06684c90480
Rishabh <lordeadroid@users.noreply.github.com>
2023-09-19 22:22:48 +0530

    Added cells in the page

--- src/game.jsx
+++ src/game.jsx
@@ -1,5 +1,20 @@
+import * as data from './cell-data.json';
+
+const Cell = ({ id, color, status }) => {
+  return <div className="cell" style={{ backgroundColor: { color } }}></div>;
+};
+
+const Cells = ({ cellDetails }) => {
+  const cells = cellDetails.map((cellDetail, index) => {
+    return <Cell key={index} cellDetail={cellDetail} />;
+  });
+
+  return <div className="cells">{cells}</div>;
+};
+
 const Game = () => {
-  return <h1>Hello</h1>;
+  const cellDetails = data.default;
+  return <Cells cellDetails={cellDetails} />;
 };
 
 export default Game;
