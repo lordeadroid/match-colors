a3e798b00b020b7d5d7d6d56c7e81c264d14ed37
lordeadroid <rishabhrawat34@gmail.com>
2024-04-20 23:04:07 +0530

    simplied counter logic

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
 
 const CountDown = () => {
-  const vals = ["Ready", "Set", "Go", ""];
-  const [started, setStarted] = useState(false);
+  const countDownVals = ["Ready", "Set", "Go"];
+  const [isGameStarted, setGameStarted] = useState(false);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (count === 2 && started === false) {
+      if (count === 2 && isGameStarted === false) {
         setCount(0);
-        setStarted(true);
+        setGameStarted(true);
         return;
       }
       setCount(count + 1);
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [count, started]);
+  }, [count, isGameStarted]);
 
   return (
-    <div className="counter">{started ? `Time: ${count}` : vals[count]}</div>
+    <div className="counter">
+      {isGameStarted ? `Time: ${count}` : countDownVals[count]}
+    </div>
   );
 };
 
